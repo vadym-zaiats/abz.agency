d7c1be65f161a293dadeb040a98c1509c5b6f08e
Vadym Zaiats <vadyk.za@gmail.com>
2023-12-14 18:27:27 +0200

    1170 ok

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"6bwWeyPh+YpDK55ShUFFaRVPcQgO+MCJCy6CNhLElz8=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"VlzJkv12nW4suSy+5IMA52VGzZvL1/XhZMiklDZYs08=\"\n}"
--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "1e7b61eb220e19ba"; }
+/******/ 		__webpack_require__.h = function() { return "990edcdc592e9d9f"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
