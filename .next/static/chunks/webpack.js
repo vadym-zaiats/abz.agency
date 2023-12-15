038e01e77f5dbdbad2d6b722f868ac0ab88bdc21
Vadym Zaiats <vadyk.za@gmail.com>
2023-12-15 16:43:28 +0200

    radio styles

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "169952e659ff8369"; }
+/******/ 		__webpack_require__.h = function() { return "863cc1160a3916db"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
